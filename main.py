1741bed88cfa143c4e7ab75ded89d4a1d86c26bc
TakahiroYoshizawa <takataku.shako.11326@gmail.com>
2017-06-15 08:48:51 +0000

    main.py を Bitbucket 上でオンラインで編集しました。

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import FFT_Forecast as ftf
 
 df = ftf.read_power_csv(csvname = './data/demand/juyo-2017.csv')
 ts = ftf.make_tswave(df)
-X  = ftf.do_fft(ts)
-ftf.est_in_lasso(X, ts)
-print(X)
+X, fft_data  = ftf.do_fft(ts)
+Lasso_coef = ftf.est_in_lasso(X, ts)
+
